166d16b73bb51d873cbbc1b19cc3686f52e54ab5
Dominik Nakamura <dnaka91@gmail.com>
2021-04-14 03:45:40 +0000

    sqlboiler: add 4.5.0 bottle.

--- Formula/sqlboiler.rb
+++ Formula/sqlboiler.rb
@@ -7,6 +7,12 @@ class Sqlboiler < Formula
   license "BSD-3-Clause"
   head "https://github.com/volatiletech/sqlboiler.git"
 
+  bottle do
+    root_url "https://github.com/dnaka91/homebrew-tap/releases/download/sqlboiler-4.5.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "2dcf26f908418f570361430a8677916ca095e96a1f3bad52fa19f7d035492a75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c7272d878a23fa8d3e446a5523b3a9640925ac92769e4a899d7268bf39474887"
+  end
+
   depends_on "go" => :build
 
   def install
