fbad7118f66c5082df6967102cf8b61cfac90e94
Dominik Nakamura <dnaka91@gmail.com>
2021-04-17 05:12:08 +0000

    toggle_cool_cow_says_type: add 0.1.9 bottle.

--- Formula/toggle_cool_cow_says_type.rb
+++ Formula/toggle_cool_cow_says_type.rb
@@ -7,6 +7,12 @@ class ToggleCoolCowSaysType < Formula
   license "MIT"
   head "https://github.com/togglebyte/toggle_cool_cow_says_type.git"
 
+  bottle do
+    root_url "https://github.com/dnaka91/homebrew-tap/releases/download/toggle_cool_cow_says_type-0.1.9"
+    sha256 cellar: :any_skip_relocation, catalina:     "1ba6dcbfa9c3124dcecadceec0f140db101e03d43d031bd74d0709df3567948c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1da0b90c58d4fe61741505e7dd4b7b60c2c579d6dc86aabb26ec962ec989f53f"
+  end
+
   depends_on "rust" => :build
 
   def install
