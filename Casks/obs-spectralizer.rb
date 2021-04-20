afd1c65b12bf268d4ca9041b85a6920058d784ef
Dominik Nakamura <dnaka91@gmail.com>
2021-04-20 19:49:29 +0900

    obs-spectralizer 1.3.4 (new cask)

---
+++ Casks/obs-spectralizer.rb
@@ -0,0 +1,13 @@
+cask "obs-spectralizer" do
+  version "1.3.4"
+  sha256 "56d1901b0de5492e3c9b3ec72d0d1ad187ead9b43985a8b0be010c587a9b6af6"
+
+  url "https://github.com/univrsal/spectralizer/releases/download/v#{version}/spectralizer.v#{version}.macos.pkg"
+  name "obs-spectralizer"
+  desc "Audio visualizer plugin for obs-studio"
+  homepage "https://github.com/univrsal/spectralizer"
+
+  pkg "spectralizer.v#{version}.macos.pkg"
+
+  uninstall pkgutil: "cf.vrsal.spectralizer"
+end
