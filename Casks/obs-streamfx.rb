78326c2d6504475b70f02245291e9583067befa2
Dominik Nakamura <dnaka91@gmail.com>
2021-04-20 16:47:26 +0900

    obs-streamfx 0.10.0b3 (new cask)

---
+++ Casks/obs-streamfx.rb
@@ -0,0 +1,13 @@
+cask "obs-streamfx" do
+  version "0.10.0b3"
+  sha256 "406dbea5803b86c84bc158158e83a13e82d312d11635cf54e42469fb6715bc8a"
+
+  url "https://github.com/Xaymar/obs-StreamFX/releases/download/#{version}/StreamFX-MacOS-Installer.pkg"
+  name "obs-streamfx"
+  desc "Adds many new effects, filters, sources, transitions and encoders to OBS Studio"
+  homepage "https://github.com/Xaymar/obs-StreamFX"
+
+  pkg "StreamFX-MacOS-Installer.pkg"
+
+  uninstall pkgutil: "com.xaymar.StreamFX"
+end
