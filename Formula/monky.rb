95d8bfaf83c335b2857359fd49d43458086af4bc
Dominik Nakamura <dnaka91@gmail.com>
2021-04-25 08:16:15 +0000

    monky: add 0.2.0 bottle.

--- Formula/monky.rb
+++ Formula/monky.rb
@@ -7,6 +7,12 @@ class Monky < Formula
   license "AGPL-3.0-only"
   head "https://github.com/dnaka91/monky.git"
 
+  bottle do
+    root_url "https://github.com/dnaka91/homebrew-tap/releases/download/monky-0.2.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "45dcf2da18aba7940724cdfd60962560a3cbdea3d0b3a211d8b64a691af7d0b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "86ce461c9b1e817bd7d1e1cb61718d707e5917296002ea1e234706ce087fe33b"
+  end
+
   depends_on "rust" => :build
 
   def install
