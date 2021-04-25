b6e8225afe534e06c3df27fdbabc8167d1512eb2
Dominik Nakamura <dnaka91@gmail.com>
2021-04-25 06:25:47 +0000

    wonky: add 0.1.0 bottle.

--- Formula/wonky.rb
+++ Formula/wonky.rb
@@ -7,6 +7,12 @@ class Wonky < Formula
   license "MIT"
   head "https://github.com/the-gorg/wonky.git"
 
+  bottle do
+    root_url "https://github.com/dnaka91/homebrew-tap/releases/download/wonky-0.1.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "bc2b2af008362238a23ae001eda24423fea0ec81da2e45119c46f415e4a64586"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bb8391e5be48f5f7b70f2ea370cd6498a0a2d096633fdf9652f42442fa82a07f"
+  end
+
   depends_on "rust" => :build
 
   def install
