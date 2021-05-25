5b3d6192ac5c4bea2de7f4b9a398a5d8b099cf35
Dominik Nakamura <dnaka91@gmail.com>
2021-05-25 03:21:23 +0000

    protolock: add 0.15.2 bottle.

--- Formula/protolock.rb
+++ Formula/protolock.rb
@@ -7,6 +7,12 @@ class Protolock < Formula
   license "BSD-3-Clause"
   head "https://github.com/nilslice/protolock.git"
 
+  bottle do
+    root_url "https://github.com/dnaka91/homebrew-tap/releases/download/protolock-0.15.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "36ea240323f6094089aa728cc19af67b6f666d255a7dda51abcd748d84c668f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5364f8812afae96ae23f09c7dfcdbeaef5a7367026fa6efad086467d5fb53ce9"
+  end
+
   depends_on "go" => :build
 
   def install
