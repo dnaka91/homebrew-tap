df8713d8c7f90665575b53efc60e2be001761d64
Dominik Nakamura <dnaka91@gmail.com>
2021-05-25 12:17:35 +0900

    obs-move-transition 2.4.1
    Closes #21.
    
    Signed-off-by: Dominik Nakamura <dnaka91@gmail.com>

--- Casks/obs-move-transition.rb
+++ Casks/obs-move-transition.rb
@@ -1,14 +1,14 @@
 cask "obs-move-transition" do
-  version "2.4.0,3283"
-  sha256 "eff010859539bde7fa0f24b55125290b0715d792f867e5f0940ec2f23a70c4ad"
+  version "2.4.1,3364"
+  sha256 "cb0bde2cabcbff2a0763f82ffb9c9ee5b77e456f0e728919881dc6ead9322b36"
 
-  url "https://obsproject.com/forum/resources/move-transition.913/version/#{version.after_comma}/download?file=70033",
+  url "https://obsproject.com/forum/resources/move-transition.913/version/#{version.after_comma}/download?file=71064",
       verified: "https://obsproject.com/forum/resources/move-transition.913/"
   name "obs-move-transition"
   desc "Move transition for OBS Studio"
   homepage "https://github.com/exeldro/obs-move-transition"
 
-  pkg "move-transition-2021-04-14-cccb025d71658d8db0b98910ce8dc9ee27fd3e51-macos.pkg"
+  pkg "move-transition-2021-05-13-d4ec1679f390b7de5e44dd6e752b3e53523e71ca-macos.pkg"
 
   uninstall pkgutil: "com.exeldro.move-transition"
 end
